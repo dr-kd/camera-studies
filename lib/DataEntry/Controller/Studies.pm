9a70b6fc1c92fee252f89ea97e1b9dc61b4e2ddb
Kieren Diment <kd@fenchurch.local>
2009-09-25 09:10:37 +1000

    correct rendering of m2m and 12m columns

--- lib/DataEntry/Controller/Studies.pm
+++ lib/DataEntry/Controller/Studies.pm
@@ -8,16 +8,18 @@ sub item : Chained('start') PathPart('') CaptureArgs(1) {
     my ($self, $c, $id) = @_;
     $c->stash(
         s => $c->model("DB::Studies")->find($id),
-        details => $c->model('DB::DetailsView')->search({}, { bind => [ $id ] }),
-        species => $c->model('DB::SpeciesView')->search({}, { bind => [ $id ] }),
-        placement => $c->model('DB::PlacementView')->search({}, { bind => [ $id ] }),
-        field => $c->model('DB::FieldView')->search({}, { bind => [ $id ] }),
-        design => $c->model('DB::DesignView')->search({}, { bind => [ $id ] }),
-        camera => $c->model('DB::CameraView')->search({}, { bind => [ $id ] }),
-        analy => $c->model('DB::AnalyView')->search({}, { bind => [ $id ] }),
-        results => $c->model('DB::Results')->search({}),
-        extras => $c->model('DB::Extra')->search({}),
+        details => $c->model('DB::DetailsView')->search_rs({}, { bind => [ $id ] }),
+        species => $c->model('DB::SpeciesView')->search_rs({}, { bind => [ $id ] }),
+        placement => $c->model('DB::PlacementView')->search_rs({}, { bind => [ $id ] }),
+        field => $c->model('DB::FieldView')->search_rs({}, { bind => [ $id ] }),
+        design => $c->model('DB::DesignView')->search_rs({}, { bind => [ $id ] }),
+        camera => $c->model('DB::CameraView')->search_rs({}, { bind => [ $id ] }),
+        analy => $c->model('DB::AnalyView')->search_rs({}, { bind => [ $id ] }),
+        results => $c->model('DB::Results')->search_rs({}),
+        extras => $c->model('DB::Extra')->search_rs({}),
     );
+    $DB::single=1;
+    warn "leaving controller";
 }
 
 1;
