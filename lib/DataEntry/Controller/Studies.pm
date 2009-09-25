e809e8874d078dd965a0153dcff695d6653cd35b
Kieren Diment <kd@fenchurch.local>
2009-09-26 00:10:51 +1000

    get data

--- lib/DataEntry/Controller/Studies.pm
+++ lib/DataEntry/Controller/Studies.pm
@@ -4,18 +4,20 @@ BEGIN { extends 'DataEntry::ControllerBase::Table' };
 __PACKAGE__->config->{action}->{start}->{PathPart} = 'studies';
 __PACKAGE__->config->{template_dir} = 'studies';
 
+use URI;
+
 sub item : Chained('start') PathPart('') CaptureArgs(1) {
     my ($self, $c, $id) = @_;
     $c->stash(
         id => $id,
         s => $c->model("DB::Studies")->find($id),
-        details => $c->model('DB::DetailsView')->search_rs({}, { bind => [ $id ] }),
-        species => $c->model('DB::SpeciesView')->search_rs({}, { bind => [ $id ] }),
-        placement => $c->model('DB::PlacementView')->search_rs({}, { bind => [ $id ] }),
-        field => $c->model('DB::FieldView')->search_rs({}, { bind => [ $id ] }),
-        design => $c->model('DB::DesignView')->search_rs({}, { bind => [ $id ] }),
-        camera => $c->model('DB::CameraView')->search_rs({}, { bind => [ $id ] }),
-        analy => $c->model('DB::AnalyView')->search_rs({}, { bind => [ $id ] }),
+        details => $c->model('DB::DetailsView')->search_rs({}, { bind => [ $id, $id ] }),
+        species => $c->model('DB::SpeciesView')->search_rs({}, { bind => [ $id, $id ] }),
+        placement => $c->model('DB::PlacementView')->search_rs({}, { bind => [ $id, $id ] }),
+        field => $c->model('DB::FieldView')->search_rs({}, { bind => [ $id, $id ] }),
+        design => $c->model('DB::DesignView')->search_rs({}, { bind => [ $id, $id ] }),
+        camera => $c->model('DB::CameraView')->search_rs({}, { bind => [ $id, $id ] }),
+        analy => $c->model('DB::AnalyView')->search_rs({}, { bind => [ $id, $id ] }),
         results => $c->model('DB::Results')->search_rs({study_id => $id}),
         extras => $c->model('DB::Extra')->search_rs({study_id => $id}),
     );
