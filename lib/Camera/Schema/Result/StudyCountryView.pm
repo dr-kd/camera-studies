a532b21c279a3b51eea5b234633cf19be8721727
Kieren Diment <kd@fenchurch.local>
2009-09-29 15:47:01 +1000

    countries ordering saner

--- lib/Camera/Schema/Result/StudyCountryView.pm
+++ lib/Camera/Schema/Result/StudyCountryView.pm
@@ -10,7 +10,7 @@ __PACKAGE__->load_components('Core');
 __PACKAGE__->table_class('DBIx::Class::ResultSource::View');
 __PACKAGE__->table('studycountry_view');
 __PACKAGE__->result_source_instance->is_virtual(1);
-__PACKAGE__->result_source_instance->view_definition( $util->join_view('study_country', 'studies', 'studies_countries', 'countries_id' ));
+__PACKAGE__->result_source_instance->view_definition( $util->join_view_country('study_country', 'studies', 'studies_countries', 'countries_id' ));
 
 use Camera::Schema::Result::StudyCountry;
 use Camera::Schema::Result::Studies;
--- lib/Camera/Schema/Util.pm
+++ lib/Camera/Schema/Util.pm
@@ -11,5 +11,16 @@ sub join_view {
     return $sql;
 }
 
+sub join_view_country {
+    my ($self, $left, $right, $join, $fk) = @_;
+    my $sql = qq{select $left.*, $join.study_id from $left
+    left outer join $join
+    on $join.$fk = $left.id and study_id = ?
+    order by study_id desc, country
+    };
+    return $sql;
+}
+
+
 __PACKAGE__->meta->make_immutable;
 1;
